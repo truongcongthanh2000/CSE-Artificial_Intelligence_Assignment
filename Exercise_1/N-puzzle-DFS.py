6149101c504d2bdbf82f49cfb51b02b6f9b54557
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2021-04-05 11:36:38 +0700

    add solution DFS with no Recursive to solve N puzzle

--- Exercise_1/N-puzzle-DFS.py
+++ Exercise_1/N-puzzle-DFS.py
@@ -3,248 +3,431 @@ import math
 import timeit
 from collections import Counter
 from random import randint
-from numpy import random
 import numpy as np
+import random
 
-class SegmentTree:
-    def __init__(self, n):
+# 2 way to run code:
+# python N-puzzle-DFS-sol.py (numRow) noRandom (numMoves) ('Recursive', 'noRecursive')
+# python N-puzzle-DFS-sol.py (numRow) Random (numMoves) ('Recursive', 'noRecursive')
+# best Solution: Choose noRecursive to avoid use lot of memory
+
+class N_PuzzleState:
+    """
+    The Eight Puzzle is described in the course textbook on
+    page 64.
+
+    This class defines the mechanics of the puzzle itself.  The
+    task of recasting this puzzle as a search problem is left to
+    the EightPuzzleSearchProblem class.
+    """
+
+    def __init__(self, numbers, n):
+        """
+          Constructs a new eight puzzle from an ordering of numbers.
+
+        numbers: a list of integers from 0 to 8 representing an
+          instance of the eight puzzle.  0 represents the blank
+          space.  Thus, the list
+
+            [1, 0, 2, 3, 4, 5, 6, 7, 8]
+
+          represents the eight puzzle:
+            -------------
+            | 1 |   | 2 |
+            -------------
+            | 3 | 4 | 5 |
+            -------------
+            | 6 | 7 | 8 |
+            ------------
+
+        The configuration of the puzzle is stored in a 2-dimensional
+        list (a list of lists) 'cells'.
+        """
+        self.cells = []
+        numbers = numbers[:] # Make a copy so as not to cause side-effects.
+        numbers.reverse()
         self.n = n
-        self.IT = [0 for i in range(n * 4)]
-    
-    def update(self, i, L, R, u):
-        if (L > u) or (R < u):
-            return None
-        if L == R:
-            self.IT[i] = 1
-            return None
-        mid = (L + R) >> 1
-        self.update(i << 1, L, mid, u)
-        self.update(i << 1 | 1, mid + 1, R, u)
-        self.IT[i] = self.IT[i << 1] + self.IT[i << 1 | 1]
-        
-    def count(self, i, L, R, u, v):
-        if (L > v) or (R < u):
-            return 0
-        if L >= u and R <= v:
-            return self.IT[i]
-        mid = (L + R) >> 1
-        left = self.count(i << 1, L, mid, u, v)
-        right = self.count(i << 1 | 1, mid + 1, R, u, v)
-        return left + right
-    
-    def add(self, x):
-        self.update(1, 0, self.n - 1, x)
+        for row in range( self.n ):
+            self.cells.append( [] )
+            for col in range( self.n ):
+                self.cells[row].append( numbers.pop() )
+                if self.cells[row][col] == 0:
+                    self.blankLocation = row, col
 
-    def number_Bigger(self, x):
-        return self.count(1, 0, self.n - 1, x + 1, self.n - 1)
+    def isGoal( self ):
+        """
+          Checks to see if the puzzle is in its goal state.
+          For example, n = 3:
+            -------------
+            |   | 1 | 2 |
+            -------------
+            | 3 | 4 | 5 |
+            -------------
+            | 6 | 7 | 8 |
+            -------------
 
+        >>> EightPuzzleState([0, 1, 2, 3, 4, 5, 6, 7, 8]).isGoal()
+        True
 
-def count_NumInversion(arr):
-    n = len(arr)
-    invCount = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            if arr[i] > 0 and arr[j] > 0 and arr[j] < arr[i]:
-                invCount += 1
-    return invCount
-    # ST = SegmentTree(n)
-    # ans = 0
-    # for x in arr:
-    #     if x > 0:
-    #         num = ST.number_Bigger(x)
-    #         ans += num
-    #     ST.add(x)
-    # return ans
-
-# How to check if an instance of N puzzle is solvable
-# https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
-def isSolvable(puzzle):
-    sz = len(puzzle)
-    n = int(math.sqrt(sz))
-    assert n * n == sz, "isSolvable -> sqrt: failed"
-    # arr = [puzzle[i][j] for i in range(n) for j in range(n)]
-    # print("arr = ", arr)
-    # invCount = count_NumInversion(arr)
-    invCount = count_NumInversion(puzzle)
-    if n % 2:
-        if invCount % 2:
-            return False
-        else:
-            return True
-    else:
-        pos = 0
-        for i in range(n):
-            for j in range(n):
-                if puzzle[i * n + j] == 0:
-                    pos = n - i
-                    break
-        if pos % 2:
-            if invCount % 2:
-                return False
-            else:
-                return True
-        else:
-            if invCount % 2:
-                return True
-            else:
-                return False
-
-def gen(n = 4):
-    perm = np.random.permutation(n * n)
-    return list(perm)
-    # print(perm)
-    # print(count_NumInversion(perm))
-    # ans = [[perm[i * n + j] for j in range(n)] for i in range(n)]
-    # return ans
-
-
-def printTable(arr):
-    sz = len(arr)
-    n = int(math.sqrt(sz))
-    assert n * n == sz, "printTable -> sqrt: failed"
-    for i in range(n):
-        for j in range(n):
-            print(arr[i * n + j], end = ' ')
-        print()
-
-list_puzzle = []
-
-def printPath():
-    print("Result: " )
-    number_Puzzle = len(list_puzzle)
-    print("Start Puzzle:")
-    printTable(list_puzzle[0][0])
-    print()
-
-    if number_Puzzle == 1:
-        print("Final Puzzle:")
-        printTable(list_puzzle[0][0])
-        return None
-
-    for i in range(1, number_Puzzle - 1):
-        print("Swap: " + str(list_puzzle[i - 1][1]) + ", " + str(list_puzzle[i - 1][2]))
-        printTable(list_puzzle[i][0])
-        print()
-    
-    print("Swap: " + str(list_puzzle[number_Puzzle - 2][1]) + ", " + str(list_puzzle[number_Puzzle - 2][2]) + " -> Final Puzzle:")
-    printTable(list_puzzle[number_Puzzle - 1][0])
-
-    return None
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-def inTable(x, y, n):
-    return x >= 0 and x < n and y >= 0 and y < n
-
-def check_if_exactly_equal(list_1, list_2):
-    # check if both the lists are of same size
-    if len(list_1) != len(list_2):
-        return False
-    # create a zipped object from 2lists
-    final_list = zip(list_1, list_2)
-    # iterate over the zipped object
-    for elem in final_list:
-        if elem[0] != elem[1]:
-            return False
-    return True
-
-class MapList:
-    mod = 10**6 + 3
-    adj = [[] for i in range(mod)]
-    base = 311
-
-    def hash(self, cur_list):
-        ans = 0
-        for x in cur_list:
-            ans = (ans * self.base + x) % self.mod 
-        return ans
-    
-    def insert(self, cur_list):
-        key = self.hash(cur_list)
-        self.adj[key].append(cur_list)
-    
-    def find(self, cur_list):
-        key = self.hash(cur_list)
-        for lst in self.adj[key]:
-            if check_if_exactly_equal(lst, cur_list):
-                return True
-            # if lst == cur_list:
-            #     return True
-        return False
-
-isVisited = MapList()
-def DFS(cur, goal, p_index, n):
-    # print(cur)
-    isVisited.insert(cur)
-    list_puzzle.append([cur, -1, -1])
-    
-    if check_if_exactly_equal(cur, goal):
-        printPath()
+        >>> EightPuzzleState([1, 0, 2, 3, 4, 5, 6, 7, 8]).isGoal()
+        False
+        """
+        current = 0
+        for row in range( self.n ):
+            for col in range( self.n ):
+                if current != self.cells[row][col]:
+                    return False
+                current += 1
         return True
 
-    p0 = p_index[0]
-    x = p0 // n
-    y = p0 % n
-    for dir in range(4):
-        next_x = x + dx[dir]
-        next_y = y + dy[dir]
-        if inTable(next_x, next_y, n):
-            idx = next_x * n + next_y
-            # assert idx >= 0 and idx < n * n, "Out of Index"
-            # assert len(cur) == n * n, "Size Array Not Equal N x N"
-            value = cur[idx]
-            
-            new_cur = cur.copy()
-            new_cur[x * n + y], new_cur[idx] = new_cur[idx], new_cur[x * n + y]
-            p_index[0], p_index[value] = p_index[value], p_index[0]
-            list_puzzle[len(list_puzzle) - 1][1] = 0
-            list_puzzle[len(list_puzzle) - 1][2] = value
-            
-            checkVisited = isVisited.find(new_cur)
-            if checkVisited == False and DFS(new_cur, goal, p_index, n):
-                return True
+    def legalMoves( self ):
+        """
+          Returns a list of legal moves from the current state.
 
-            p_index[0], p_index[value] = p_index[value], p_index[0]
-            list_puzzle[len(list_puzzle) - 1][1] = -1
-            list_puzzle[len(list_puzzle) - 1][2] = -1
+        Moves consist of moving the blank space up, down, left or right.
+        These are encoded as 'up', 'down', 'left' and 'right' respectively.
+
+        >>> EightPuzzleState([0, 1, 2, 3, 4, 5, 6, 7, 8]).legalMoves()
+        ['down', 'right']
+        """
+        moves = []
+        row, col = self.blankLocation
+        if(row != 0):
+            moves.append('up')
+        if(row != self.n - 1):
+            moves.append('down')
+        if(col != 0):
+            moves.append('left')
+        if(col != self.n - 1):
+            moves.append('right')
+        return moves
+
+    def result(self, move):
+        """
+          Returns a new eightPuzzle with the current state and blankLocation
+        updated based on the provided move.
+
+        The move should be a string drawn from a list returned by legalMoves.
+        Illegal moves will raise an exception, which may be an array bounds
+        exception.
+
+        NOTE: This function *does not* change the current object.  Instead,
+        it returns a new object.
+        """
+        row, col = self.blankLocation
+        if(move == 'up'):
+            newrow = row - 1
+            newcol = col
+        elif(move == 'down'):
+            newrow = row + 1
+            newcol = col
+        elif(move == 'left'):
+            newrow = row
+            newcol = col - 1
+        elif(move == 'right'):
+            newrow = row
+            newcol = col + 1
+        else:
+            raise "Illegal Move"
+
+        # Create a copy of the current eightPuzzle
+        newPuzzle = N_PuzzleState([0 for i in range(self.n * self.n)], self.n)
+        newPuzzle.cells = [values[:] for values in self.cells]
+        # And update it to reflect the move
+        newPuzzle.cells[row][col] = self.cells[newrow][newcol]
+        newPuzzle.cells[newrow][newcol] = self.cells[row][col]
+        newPuzzle.blankLocation = newrow, newcol
+
+        return newPuzzle
+
+    # Utilities for comparison and display
+    def __eq__(self, other):
+        """
+            Overloads '==' such that two eightPuzzles with the same configuration
+          are equal.
+
+          >>> EightPuzzleState([0, 1, 2, 3, 4, 5, 6, 7, 8]) == \
+              EightPuzzleState([1, 0, 2, 3, 4, 5, 6, 7, 8]).result('left')
+          True
+        """
+        for row in range( self.n ):
+            if self.cells[row] != other.cells[row]:
+                return False
+        return True
+
+    def __hash__(self):
+        return hash(str(self.cells))
+
+    def __getAsciiString(self):
+        """
+          Returns a display string for the maze
+        """
+        lines = []
+        horizontalLine = ('-' * (13))
+        lines.append(horizontalLine)
+        for row in self.cells:
+            rowLine = '|'
+            for col in row:
+                if col == 0:
+                    col = ' '
+                rowLine = rowLine + ' ' + col.__str__() + ' |'
+            lines.append(rowLine)
+            lines.append(horizontalLine)
+        return '\n'.join(lines)
+
+    def __str__(self):
+        return self.__getAsciiString()
+
+class N_PuzzleStorage:
+    def __init__(self):
+        self.isVisited = {}
     
-    list_puzzle.pop()
-    return False
+    def add(self, state):
+        # print("Add: ")
+        # print(state)
+        self.isVisited[state] = True
+    
+    def isVisit(self, state):
+        if state in self.isVisited:
+            return True
+        else:
+            return False
 
-list_canSolved = [
-    [1, 2, 3, 4, 6, 0, 7, 5, 8], 
-    [7, 5, 8, 3, 0, 1, 2, 6, 4]
-]
-def main():
-    print(sys.getrecursionlimit()) # = 1000
-    # sys.setrecursionlimit(12345678) 
+# https://www.geeksforgeeks.org/stack-in-python/
+# Python program to demonstrate
+# stack implementation using a linked list.
+# node class
+class Node:
+   def __init__(self, value):
+      self.value = value
+      self.next = None
+ 
+class Stack:
+    
+   # Initializing a stack.
+   # Use a dummy node, which is
+   # easier for handling edge cases.
+   def __init__(self):
+      self.head = Node("head")
+      self.size = 0
+ 
+   # String representation of the stack
+   def __str__(self):
+      cur = self.head.next
+      out = ""
+      while cur:
+         out += str(cur.value) + "->"
+         cur = cur.next
+      return out[:-3]  
+ 
+   # Get the current size of the stack
+   def getSize(self):
+      return self.size
+    
+   # Check if the stack is empty
+   def isEmpty(self):
+      return self.size == 0
+    
+   # Get the top item of the stack
+   def peek(self):
+       
+      # Sanitary check to see if we
+      # are peeking an empty stack.
+      if self.isEmpty():
+         raise Exception("Peeking from an empty stack")
+      return self.head.next.value
+ 
+   # Push a value into the stack.
+   def push(self, value):
+      node = Node(value)
+      node.next = self.head.next
+      self.head.next = node
+      self.size += 1
+      
+   # Remove a value from the stack and return.
+   def pop(self):
+      if self.isEmpty():
+         raise Exception("Popping from an empty stack")
+      remove = self.head.next
+      self.head.next = self.head.next.next
+      self.size -= 1
+      return remove.value
 
-    n = 3
-    puzzle = gen(n)
-    # puzzle = [1, 2, 3, 4, 6, 0, 7, 5, 8]
-    p_index = [0 for i in range(n * n)]
-    for i in range(n * n):
-        p_index[puzzle[i]] = i
-    print(puzzle)
+class N_PuzzleSearchProblem():
+    """
+      Implementation of a SearchProblem for the  Eight Puzzle domain
 
-    canSolve = isSolvable(puzzle)
-    print(canSolve)
-    printTable(puzzle)
+      Each state is represented by an instance of an eightPuzzle.
+    """
+    def __init__(self, puzzle, n):
+        "Creates a new EightPuzzleSearchProblem which stores search information."
+        self.puzzle = puzzle
+        self.n = n
+        self.MapPuzzle = N_PuzzleStorage()
+        self.list_action = []
+        self.n_DFS = 0
 
-    goal = [i for i in range(1, n * n)]
-    goal.append(0)
-    print(goal)
+    def getStartState(self):
+        return self.puzzle
 
-    assert DFS(puzzle, goal, p_index, n) == canSolve, "Error to Solve: " + str(canSolve)
+    def isGoalState(self,state):
+        return state.isGoal()
 
+    def DFS_noRecursive(self, state):
+        stk = Stack()
+        stk.push(state)
+
+        while stk.isEmpty() == False:
+            cur = stk.pop()
+            self.MapPuzzle.add(cur)
+
+            if self.isGoalState(cur):
+                return True
+            hasChoose = False
+            for a in cur.legalMoves():
+                successor = cur.result(a)
+                if self.MapPuzzle.isVisit(successor) == False:
+                    self.list_action.append(a)
+                    stk.push(cur)
+                    stk.push(successor)
+                    hasChoose = True
+                    break
+            if hasChoose == False:
+                if len(self.list_action) > 0:
+                    self.list_action.pop()
+    
+    def DFS_Recursive(self, state):
+        self.n_DFS += 1
+        # print("DFS = ", self.n_DFS)
+        # print(state)
+        self.MapPuzzle.add(state)
+        if self.isGoalState(state):
+            return True
+
+        if self.n_DFS < 3500: # Magic!
+            for a in state.legalMoves():
+                # print(a)
+                successor = state.result(a)
+                # print(successor)
+                if self.MapPuzzle.isVisit(successor) == False:
+                    self.list_action.append(a)
+                    if self.DFS_Recursive(successor):
+                        return True
+                    self.list_action.pop()
+        self.n_DFS -= 1
+        return False
+
+    def getSuccessors(self,state):
+        """
+          Returns list of (successor, action, stepCost) pairs where
+          each succesor is either left, right, up, or down
+          from the original state and the cost is 1.0 for each
+        """
+        succ = []
+        for a in state.legalMoves():
+            succ.append((state.result(a), a, 1))
+        return succ
+    
+    def getListActions(self):
+        return self.list_action
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
+        """
+        return len(actions)
+
+LIST_PUZZLE_DATA = [[1, 0, 2, 3, 4, 5, 6, 7, 8],
+                     [1, 7, 8, 2, 3, 4, 5, 6, 0],
+                     [4, 3, 2, 7, 0, 5, 1, 6, 8],
+                     [5, 1, 3, 4, 0, 2, 6, 7, 8],
+                     [1, 2, 5, 7, 6, 8, 0, 4, 3],
+                     [0, 3, 1, 6, 8, 2, 7, 5, 4]]
+
+def loadEightPuzzle(puzzleNumber):
+    """
+      puzzleNumber: The number of the eight puzzle to load.
+
+      Returns an eight puzzle object generated from one of the
+      provided puzzles in EIGHT_PUZZLE_DATA.
+
+      puzzleNumber can range from 0 to 5.
+
+      >>> print(loadEightPuzzle(0))
+      -------------
+      | 1 |   | 2 |
+      -------------
+      | 3 | 4 | 5 |
+      -------------
+      | 6 | 7 | 8 |
+      -------------
+    """
+    puzzle = LIST_PUZZLE_DATA[puzzleNumber]
+    n = int(math.sqrt(len(puzzle)))
+    print(puzzle, n)
+    return N_PuzzleState(puzzle, n)
+
+def createRandomN_Puzzle(n, moves = 100):
+    """
+      moves: number of random moves to apply
+
+      Creates a random eight puzzle by applying
+      a series of 'moves' random moves to a solved
+      puzzle.
+    """
+    puzzle = N_PuzzleState([i for i in range(n * n)], n)
+    for i in range(moves):
+        # Execute a random legal move
+        puzzle = puzzle.result(random.sample(puzzle.legalMoves(), 1)[0])
+    return puzzle
+
+def main(n, isRandom, moves, isRecursive):
+    print("Number Recursion = ", sys.getrecursionlimit()) # = 1000
+    sys.setrecursionlimit(12345678) 
+
+    if isRandom == False:
+        n = 3
+        choose = random.randint(0, 5)
+        # choose = 1 # Thay đổi chọn puzzle 1 -> đệ quy lâu hơn
+
+        print("Choose puzzle = ", choose, file = sys.stderr)
+        problem = N_PuzzleSearchProblem(loadEightPuzzle(choose), n)
+    else:
+        problem = N_PuzzleSearchProblem(createRandomN_Puzzle(n, moves), n)
+
+    startState = problem.getStartState()
+
+    print(startState)
+    
+    canSolved = False
+    if isRecursive == True:
+        canSolved = problem.DFS_Recursive(startState)
+    else:
+        canSolved = problem.DFS_noRecursive(startState)
+
+    if canSolved:
+        print("Problem is Solvable: ")
+        path = problem.getListActions()
+
+        i = 1
+        for a in path:
+            startState = startState.result(a)
+            print('After %d move%s: %s' % (i, ("", "s")[i>1], a))
+            print(startState)
+
+            i += 1
+    else:
+        print("Problem may have no solution: ")
 
 if __name__ == '__main__':
     start = timeit.default_timer()
 
     numTest = 1
     while numTest:
-        main()
+        main(int(sys.argv[1]), sys.argv[2] == "Random", int(sys.argv[3]), sys.argv[4] == "Recursive")
         numTest -= 1
 
     stop = timeit.default_timer()
